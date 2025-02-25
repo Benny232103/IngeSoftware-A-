20c8cf210675de8d6f059f143f893005e56f0ada
cavaanna <131871765+cavaanna@users.noreply.github.com>
2025-02-25 22:44:45 +0100

    Update GestoreMain.java
    ho sistemato un attimo la verifica dell'utente e ho fatto in modo di modificare successivamente utente e password

--- GestoreVisite/it/unibs/ingesw/gestvisit/GestoreMain.java
+++ GestoreVisite/it/unibs/ingesw/gestvisit/GestoreMain.java
@@ -1,11 +1,18 @@
     import java.util.*;
     public class GestoreMain {
     public static void main(String[] args) {
+        
         Configuratore config = new Configuratore("admin", "password123");
         GestoreVisite gestore = new GestoreVisite();
-
-        if (config.login("admin", "password123")) {
-            System.out.println("Accesso effettuato con successo!");
+        System.out.println("buongiorno, per poter accedere inserire");
+	    Configuratore configuratoreUtente = Configuratore.creaConfiguratore(); 
+	        
+	       
+	     if (configuratoreUtente.login(config.getUsername(),config.getPassword())) {
+	        System.out.println("Accesso effettuato con successo!");
+	        System.out.println("si prega di modificare il nome utente e la password");
+	        configuratoreUtente = configuratoreUtente.creaConfiguratore();
+             
             gestore.aggiungiLuogo("Museo Storico");
             gestore.modificaMaxPersone(15);
             gestore.aggiungiDataNonDisponibile("2025-06-10");
