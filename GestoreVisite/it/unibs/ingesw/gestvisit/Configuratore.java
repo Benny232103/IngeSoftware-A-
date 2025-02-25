83bcbb8ee4e73b72d9e67912f6643bc69ee50f93
cavaanna <131871765+cavaanna@users.noreply.github.com>
2025-02-25 22:39:42 +0100

    Update Configuratore.java
    ho inserito getters and setters e il metodo creaConfiguratore

--- GestoreVisite/it/unibs/ingesw/gestvisit/Configuratore.java
+++ GestoreVisite/it/unibs/ingesw/gestvisit/Configuratore.java
@@ -1,20 +1,51 @@
 import java.util.*;
 class Configuratore {
-    private String username;
-    private String password;
-    private boolean isLogged;
+   
+        private String username;
+	    private String password;
+	    private boolean isLogged;
 
-    public Configuratore(String username, String password) {
-        this.username = username;
-        this.password = password;
-        this.isLogged = false;
-    }
+	    public Configuratore(String username, String password) {
+	        this.username = username;
+	        this.password = password;
+	        this.isLogged = false;
+	    }
 
-    public boolean login(String username, String password) {
-        if (this.username.equals(username) && this.password.equals(password)) {
-            isLogged = true;
-            return true;
-        }
-        return false;
-    }
+	    public boolean login(String username, String password) {
+	        if (this.username.equals(username) && this.password.equals(password)) {
+	            isLogged = true;
+	            return true;
+	        }
+	        return false;
+	    }
+	    
+	    public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public boolean isLogged() {
+			return isLogged;
+		}
+
+		public void setLogged(boolean isLogged) {
+			this.isLogged = isLogged;
+		}
+
+		public static Configuratore creaConfiguratore() {
+	    	 String accessoConfiguratoreString = InputDati.leggiStringaNonVuota("nome utente: ");
+		     String passwordConfiguratoreString = InputDati.leggiStringaNonVuota("password: ");
+		     return new Configuratore(accessoConfiguratoreString, passwordConfiguratoreString);
+	    }
 }
