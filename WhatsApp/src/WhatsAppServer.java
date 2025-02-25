81570bb1ee1758741f3eec60c74c6c3c3a022a07
Jocke404 <ruggero.lombardi8@gmail.com>
2025-02-25 21:39:22 +0100

    First commit

---
+++ WhatsApp/src/WhatsAppServer.java
@@ -0,0 +1,28 @@
+import java.net.*;
+
+public class WhatsAppServer {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Avvio del server...");
+		
+		try(
+			ServerSocket server = new ServerSocket(1234);
+		){
+			System.out.println("Server avviato...");
+			
+			while(true) {
+				System.out.println("Attesa client...");
+				Socket client = server.accept();
+				
+				WhatsAppProtocol p = new WhatsAppProtocol(client);
+				Thread t = new Thread(p);
+				t.start();
+			}
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+}
